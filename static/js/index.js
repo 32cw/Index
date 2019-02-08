9b22e3f899847471a4c993bae8016c1e97a60f00
随心 <1107224733@qq.com>
2019-02-08 17:36:28 +0800

    修改Live2D菜单颜色

--- static/js/index.js
+++ static/js/index.js
@@ -43,7 +43,7 @@ if (isMobile()) {
     });
     // 加载Live2D
     try {
-        $('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+        $('body').append('<style>.waifu-tool span{display:block;cursor:pointer;color:#f3f9f1;transition:.2s}</style><div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
         /* 可直接修改部分参数 */
         live2d_settings['modelId'] = 6;                  // 默认模型 ID
         live2d_settings['modelTexturesId'] = 3;         // 默认材质 ID
