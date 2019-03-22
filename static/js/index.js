d5344f9411907e2caa59a0e5dac2461f58a448c1
wang926454 <1107224733@qq.com>
2019-03-22 19:02:31 +0800

    更新live2d

--- static/js/index.js
+++ static/js/index.js
@@ -46,7 +46,7 @@ if (isMobile()) {
         $('body').append('<style>.waifu-tool span{display:block;cursor:pointer;color:#f3f9f1;transition:.2s}</style><div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
         /* 可直接修改部分参数 */
         live2d_settings['modelId'] = 6;                  // 默认模型 ID
-        live2d_settings['modelTexturesId'] = 3;         // 默认材质 ID
+        live2d_settings['modelTexturesId'] = 2;         // 默认材质 ID
         live2d_settings['modelStorage'] = false;         // 不储存模型 ID
         live2d_settings['canTurnToHomePage'] = false;    // 隐藏 返回首页 按钮
         live2d_settings['waifuEdgeSide'] = 'right:30';   // 看板娘贴边方向
